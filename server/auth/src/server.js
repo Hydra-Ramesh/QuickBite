dbeddde51414adfcd04f9141c6b590b804628642
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2026-02-14 02:50:29 +0530

    Add Router

--- server/auth/src/server.js
+++ server/auth/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.connect.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
 
 app.use((req, res, next) => {
   const start = Date.now();
